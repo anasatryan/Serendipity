3895a4131d895668e77ee8bec847ff781d10bcba
anasatryan <61308440+anasatryan@users.noreply.github.com>
2020-03-27 19:40:56 +0400

    Update stress_cause.json.py

--- stress_cause.json.py
+++ stress_cause.json.py
@@ -1,17 +1,20 @@
 import json
 stress_cause = input("Are you stressed?")
-if (stress_cause == "yes"):
- stressor1 = input("Is your stress caused by negative thoughts?")
- while (stressor1 == "yes"):
+while (stress_cause == "yes"):
+   stressor1 = input("Is your stress caused by negative thoughts?")
+   if (stressor1 == "yes"):
     print("read motivational articles,read some books or watch movies!")
     break
- stressor2 = input("Are you mentally and physically overwoked?")
- while(stressor2 == "yes"):
+   stressor2 = input("Are you mentally and physically overwoked?")
+   if (stressor2 == "yes"):
     print("try meditating, take your supplements, promote rest and mental relaxation")
     break
- stressor3 = input("Are you stressed because of a disease?")
- while(stressor3 == "yes"):
+   stressor3 = input("Are you stressed because of a disease?")
+   if (stressor3 == "yes"):
     print("Seek medical help from a professional or ask for help from your family and friends")
     break
-else:
-    your_stressor = input("Please provide the reason that caused your stress!")
+   else:
+    stress_cause = input("Please provide the reason that caused your stress!")
+    print(stress_cause)
+with open('my_data.json', 'w') as f:
+    json.dump(stress_cause, f)
